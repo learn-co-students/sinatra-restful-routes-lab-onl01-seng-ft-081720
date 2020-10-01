465473633a3c4d225a9a80bf164962a9a00fcfdd
Bri8102 <damioniru16@gmail.com>
2020-10-01 16:18:22 -0400

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+
+Recipe.create(name: 'French Toast', ingredients: 'Eggs, Milk, Cinnamon, Pepper', cook_time: '5')
+Recipe.create(name: 'Pancakes', ingredients: 'Eggs, Milk, Water, Sugar', cook_time: '10')
+Recipe.create(name: 'Waffles', ingredients: 'Flour, Eggs, Milk, Water, Sugar', cook_time: '15')
