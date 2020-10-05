78e6f4927f97dd49cd1695a8617cba1b5a49f736
NickRylander <nick.rylander@outlook.com>
2020-10-05 14:24:32 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,62 @@
+
+require './config/environment'
+
 class ApplicationController < Sinatra::Base
+  # register Sinatra::ActiveRecordExtension
+  set :views, Proc.new { File.join(root, "../views/") }
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :"index"
+  end
 
+  get '/recipes/new' do
+    erb :new
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+
+    erb :"show"
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect "/recipes/#{ @recipe.id }"
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])  
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect '/recipes'
+  end
 end
+
+
+#   edit page '/recipes/:id/edit'
+#     responds with a 200 status code (FAILED - 10)
+#     contains a form to edit the recipe (FAILED - 11)
+#     displays the recipe's ingredients before editing (FAILED - 12)
+#     submits via a patch request (FAILED - 13)
+#   updating a recipe
+#     updates the recipe (FAILED - 19)
+#     redirects to the recipe show page (FAILED - 20)
