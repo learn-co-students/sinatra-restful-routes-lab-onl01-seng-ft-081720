cdda152749b13b38123be20286df1e438f0e3748
ken025 <kennethmanuel250@gmail.com>
2020-12-17 13:13:27 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,52 +3,42 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  
-  get '/' do
-    
+
+  get "/recipes" do 
+    @recipes = Recipe.all
     erb :index
   end
 
-  get '/recipes' do
-    @recipes = Recipe.all
-    
-    erb :recipes
-  end
-  
-  get '/recipes/new' do
-    
+  get "/recipes/new" do
     erb :new
   end
 
-  get '/recipes/:id' do
-    @recipe = Recipe.find_by_id(params[:id])
-    
-    erb :show
+  post "/recipes" do
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect "/recipes/#{@recipe.id}"
   end
 
   get "/recipes/:id/edit" do
-    @recipe = Recipe.find(params[:id])
-    
+    @recipe = Recipe.find_by_id(params[:id])
     erb :edit
   end
 
-  post '/recipes' do
-    @recipe = Recipe.create(params)
-    
-    redirect to "/recipes/#{@recipe.id}"
-  end
-
   patch "/recipes/:id" do
-    @recipe = recipe.find(params[:id])
-    @recipe.update(params[:recipe])
-    
-    redirect to "/recipes/#{@recipe.id}"
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect "/recipes/#{params[:id]}"
   end
 
-  delete '/recipes/:id' do 
+  get "/recipes/:id" do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  delete "/recipes/:id" do
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.destroy
-    
-    redirect to '/recipes'
   end
 end
